10ef52bbad4d79b00c45f88423cbc9213afacf85
jenskromdijk <jenskromdijk@gmail.com>
2025-03-17 21:04:32 +0000

    Add static counter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 
 function Page() {
   return (
@@ -6,10 +7,23 @@ function Page() {
   )
 }
 
+function DannyGo() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button className="DannyGo" onClick={handleClick}>Clicked {count} times</button>
+  )
+}
+
 export default function App() {
   return (
     <div className="App">
       <Page />
+      <DannyGo />
     </div>
   );
 }
