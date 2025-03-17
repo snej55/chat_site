99e1ac912d39117ccb95dc896925793e7343fac4
jenskromdijk <jenskromdijk@gmail.com>
2025-03-17 14:48:27 +0000

    Start react thing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Page() {
+  return (
+    <h1 className="Page">Chatbox</h1>
+  )
+}
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Page />
     </div>
   );
 }
-
-export default App;
