fafa6d5a9caddd0ed72d1aed0a2a84d8a574e900
jenskromdijk <jenskromdijk@gmail.com>
2025-03-18 14:27:31 +0000

    Fix messages

--- src/Messages.js
+++ src/Messages.js
@@ -3,20 +3,26 @@ import { useState } from 'react';
 export function MessageBox() {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([{}])
+    const [messageID, setMessageID] = useState(0);
+
+    function addMessage(e) {
+        setMessages([
+            ...messages,
+            {'content': message, 'id': messageID}
+        ]);
+        setMessageID(messageID + 1);
+    }
 
     return (
         <div>
         <h1>Messages:</h1>
-        <input className="inputBox" onChange={e => {
-            setMessage(e.target.value);
-        }}>Enter your message</input>
-        
-        <button className="messageButton" onClick={
-            setMessages([
-                ...messages,
-                {'name': message}
-            ])
-        }></button>
+        <input value={message} onChange={e => setMessage(e.target.value)} />
+        <button className="messageButton" onClick={addMessage}>Add {message}</button>
+        <ul>
+            {messages.map(message => (
+                <li key={message}>{message.content} ID: {message.id}</li>
+            ))}
+        </ul>
         </div>
     )
 }
