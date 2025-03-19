66fb808e83cb9151ddb7a59ed85512aca4e4e1ce
jenskromdijk <jenskromdijk@gmail.com>
2025-03-19 16:45:05 +0000

    Add hover effect

--- src/Messages.js
+++ src/Messages.js
@@ -1,28 +1,35 @@
 import { useState } from 'react';
+import './MessageBox.css'
 
 export function MessageBox() {
     const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([{}])
+    const [messages, setMessages] = useState([])
     const [messageID, setMessageID] = useState(0);
 
-    function addMessage(e) {
+    // message = {'content', 'id', 'username'}
+    function addMessage() {
         setMessages([
-            ...messages,
-            {'content': message, 'id': messageID}
+            {'content': message, 'id': messageID},
+            ...messages
         ]);
         setMessageID(messageID + 1);
     }
 
     return (
-        <div>
+        <div className="messageBox">
         <h1>Messages:</h1>
-        <input value={message} onChange={e => setMessage(e.target.value)} />
-        <button className="messageButton" onClick={addMessage}>Add {message}</button>
-        <ul>
+        <input value={message} className="messageInput" onChange={e => setMessage(e.target.value)}/>
+        <style>
+            {/* {1 ? (
+                
+            ) : (<div></div>)} */}
+        </style>
+        <button className="messageButton" onClick={addMessage}>Enter</button>
+        <ol>
             {messages.map(message => (
-                <li key={message}>{message.content} ID: {message.id}</li>
+                <div className="messageItem" key={message.id}>Content: '{message.content}' (ID: {message.id})</div>
             ))}
-        </ul>
+        </ol>
         </div>
-    )
+    );
 }
