6ce570e8f640c0699458f6150234d723f6729aaa
jenskromdijk <jenskromdijk@gmail.com>
2025-03-23 15:35:47 +0000

    Add socket.io

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 
 import { MessageBox } from './Messages'
+import socketClient from 'socket.io-client';
 
+const SERVER = "http://127.0.0.1:8080";
 export default function App() {
+  var socket = socketClient(SERVER);
+  socket.on('connection', () => {
+    console.log(`I'm connected!`);
+  })
+
   return (
     <div className="container">
       <MessageBox />
