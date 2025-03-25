c31ec79513600b83d2219eb2a9f2af40d346153c
zibuyin <natdrone101@gmail.com>
2025-03-25 13:04:09 +0000

    Danny GO

--- src/Messages.js
+++ src/Messages.js
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 
 import PropTypes from 'prop-types';
 
-// const socket = io("http://10.24.79.110:5001");
+//const socket = io("http://10.24.79.110:5001");
 const socket = io("http://10.24.79.194:5001");
 
 export function MessageBox({getUserName}) {
