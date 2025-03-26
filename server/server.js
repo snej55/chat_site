9e835430d76c1d8e522fe65d0b2565bce7f01236
jenskromdijk <jenskromdijk@gmail.com>
2025-03-26 10:44:56 +0000

    Upload server code

---
+++ server/server.js
@@ -0,0 +1,39 @@
+// server.js
+
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+addresses_connected = []
+
+// Handle WebSocket connections here
+io.on("connection", (socket) => {
+  console.log("A new user has connected", socket.id);
+  console.log("At ip address: ", socket.handshake.address);
+  addresses_connected.push(socket.handshake.address);
+  console.log(addresses_connected.includes(socket.handshake.address) ? "this user is already here" : "new user");
+
+  // Listen for incoming messages from clients
+  socket.on("message", (message) => {
+    // Broadcast the message to all connected clients
+    console.log(message);
+    io.emit("message", message);
+  });
+
+  // Handle disconnections
+  socket.on("disconnect", () => {
+    console.log(socket.id, " disconnected");
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
