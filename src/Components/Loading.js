d97716d27141cbf2f70d389f746d06579f231e8d
jenskromdijk <jenskromdijk@gmail.com>
2025-04-03 18:30:10 +0100

    Add loading animation

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,7 +1,24 @@
+import './Loading.css';
+
 export function LoadingPage() {
     return (
-        <div>
-            loading...
+        <div className="loading-wrapper">
+            <div className="loading-text">
+                <span className="loading-text-letter">L</span>
+                <span>o</span>
+                <span>a</span>
+                <span>d</span>
+                <span>i</span>
+                <span>n</span>
+                <span>g</span>
+                <span>.</span>
+                <span>.</span>
+                <span>.</span>
+            </div>
+            <div className="loading-support">
+                If this takes too long, try refreshing the page.
+            </div>
+            <div className="loading-text-wrapper"></div>
         </div>
     )
 }
